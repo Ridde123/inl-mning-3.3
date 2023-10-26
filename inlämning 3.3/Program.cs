af2e678b61315b75036ef53af982a40165e4980f
Pingvu <nhlpool27@gmail.com>
2023-10-26 12:41:31 +0200

    Add project files.

---
+++ inlämning 3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hur många timmar vill du hyra bilen");
+            int timmar = int.Parse(Console.ReadLine());
+            int kostnad = timmar * 80;
+            if (kostnad > 950) 
+            {
+                kostnad = 950;
+
+            }
+            Console.WriteLine($" kostnade för bilen är i {timmar} timmar är {kostnad} kr ");
+            Console.ReadKey();
+                
+         }
+    }
+    
+}
+
+
